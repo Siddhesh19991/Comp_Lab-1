ca431924612716178afd37651bb5d1025f480e40
Siddhesh Sreedar <siddheshraw@gmail.com>
2023-11-01 14:33:33 +0100

    B

--- Q1.R
+++ Q1.R
@@ -28,5 +28,30 @@ sum(2*(x-theta_i)/(1+(x-theta_i)^2))
 
 # b: Bisection
   
+a <- -4
+b <- 4 
+
+x_i <- c()
+
+while(length(x_i) < 2 || abs(tail(x_i,1)-tail(x_i,2)[1])>0.000001){
+  
+#for(i in 1:1000){   
+  y1<- sum(2*(a-theta_i)/(1+(a-theta_i)^2))
+  y2<- sum(2*(b-theta_i)/(1+(b-theta_i)^2))
+  
+  x_i <- c(x_i,(a + b)/2) 
+  
+  print((a+b)/2)
+  
+  if(y1*y2 <= 0){
+    b <- tail(x_i,1)
+  }else {
+    a <- tail(x_i,1)
+  }
+  
+}
+
+tail(x_i,1)
+abline(v=c(tail(x_i,1)))
 
 
